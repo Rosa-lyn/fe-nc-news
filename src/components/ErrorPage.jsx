7cdaa2799f4b0630e552794461e92cacbd633d90
Rosalyn Land <roz.s.land@gmail.com>
2020-09-02 15:52:44 +0100

    added error handling for user not found

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -4,12 +4,12 @@ const ErrorPage = (props) => {
   const { msg } = props;
   return (
     <div>
-      {msg === "Topic not found :(" && (
-        <section>
-          <p>Sorry, we can't find that topic!</p>
-          <img src="" alt="sad gif" />
-        </section>
-      )}
+      {msg === "Topic not found :(" && <p>Sorry, we can't find that topic!</p>}
+      {msg === "User not found :(" && <p>Sorry, we can't find that user!</p>}
+      <img
+        src="https://media.giphy.com/media/KDRv3QggAjyo/giphy.gif"
+        alt="Tobey Maguire crying GIF"
+      />
     </div>
   );
 };
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,18 +2,28 @@ import React, { Component } from "react";
 import UserProfile from "./UserProfile";
 import Articles from "./Articles";
 import Loader from "./Loader";
+import ErrorPage from "./ErrorPage";
 import * as api from "../utils/api";
 
 class User extends Component {
   state = {
     user: {},
     isLoading: true,
+    err: null,
   };
 
   componentDidMount() {
-    this.getUser().then((user) => {
-      this.setState({ user, isLoading: false });
-    });
+    this.getUser()
+      .then((user) => {
+        this.setState({ user, isLoading: false });
+      })
+      .catch(({ response }) => {
+        console.log(response);
+        this.setState({
+          isLoading: false,
+          err: { msg: response.data.msg, status: response.status },
+        });
+      });
   }
 
   getUser() {
@@ -23,8 +33,9 @@ class User extends Component {
 
   render() {
     const { username } = this.props;
-    const { user, isLoading } = this.state;
+    const { user, isLoading, err } = this.state;
     if (isLoading) return <Loader />;
+    if (err) return <ErrorPage {...err} />;
     return (
       <div>
         <UserProfile user={user} />
